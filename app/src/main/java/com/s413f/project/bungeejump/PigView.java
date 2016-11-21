f97d846d62948765de9c7adb0569e105a0431560
Ashley Wong <ashleysssmile@gmail.com>
2016-11-21 21:28:10 +0800

    Rejected commit: Default

--- app/src/main/java/com/s413f/project/bungeejump/PigView.java
+++ app/src/main/java/com/s413f/project/bungeejump/PigView.java
@@ -1,7 +1,22 @@
 package com.s413f.project.bungeejump;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
 
 /**
  * Created by Ashley Wong on 15/11/2016.
@@ -10,7 +25,299 @@ import android.view.SurfaceView;
 public class PigView extends SurfaceView {
 
 
+    /** Delay in each animation cycle, in ms. */
+    private static final int CYCLE_DELAY = 5;
+    /** Text size. */
+    private static final int TEXT_SIZE = 24;
+
+    /** Width and height of the arena. */
+    public static int arenaWidth;
+    public static int arenaHeight;
+
+    /** Animation object, the flying android. */
+    private Pig pig;
+    /** List of obstacles objects, i.e., pairs of pipes. */
+    private Vector<Stone> stones = new Vector<Stone>();
+    /** Scrolling background of the view. */
+    Background background;
+
+    /** Timer for the game loop. */
+    private Timer timer = null;
+
+    private Context context;
+
+    /** Start time of the game. */
+    private long startTime = 0;
+    /** Pause time of the game. */
+    private long pauseTime = 0;
+    /** Total time elapsed of the game. */
+    private float totalTime = 0;
+    /** Obstacle creation time. */
+    private float obstacleCreationTime;
+
+    /** Whether the game is over. */
+    private boolean gameOver;
+
+    /** Whether the game is paused and waiting for touching to start. */
+    private boolean waitForTouch = true;
+    /** Whether the pig is jumping.**/
+    private boolean jumping = true;
+    /** initial y of the pig start jumping**/
+    private float inity  ;
+
+
+    /** Saving and handling of user input of touch events. */
+    private class UserInput {
+        /** Whether there is a user input present. */
+        boolean present = false;
+        float x;
+        /**
+         * Sets the user input mouse event for later processing. This method is
+         * called in event handlers, i.e., in the main UI thread.
+         */
+        synchronized void save(MotionEvent event) {
+
+            x = event.getX();
+            present = true;
+        }
+
+        /**
+         * Handles the user input to move the flying android upward. This method is
+         * called in the thread of the game loop.
+         */
+        synchronized void handle() {
+            if (present) {
+
+                if (waitForTouch) {  // Start of the game
+                    waitForTouch = false;
+                    startTime = System.currentTimeMillis();
+                    background.stop(true);
+                    ((AnimationDrawable)(pig.getDrawable())).start();
+
+
+                }
+                else {  // Game active
+                    pig.fly(x);
+                    if(!pig.jumping(inity)){
+                        jumping = false;
+                    }
+
+                }
+
+                present = false;
+            }
+        }
+    }
+    /** User input object of touch events. */
+    private UserInput userInput = new UserInput();
+
+    /** Task for the game loop. */
+    private class AnimationTask extends TimerTask {
+        @Override
+        public void run() {
+            userInput.handle();
+
+            if (!gameOver && !waitForTouch) {
+
+                createObstacles();
+
+                if (jumping) {
+                    pig.jump();
+                } else {
+                    pig.move();
+                }
+
+
+                // iii. If the flying android moved out from the arena, call method gameOver
+                if (pig.isOutOfArena()) {
+                    pig.jump();
+                }
+                for (int i = 0; i < stones.size(); i++) {
+                    // a. Move the obstacles
+                    stones.get(i).move();
+
+                    // b. Determine if the flying android collided with any obstacle
+                    if (stones.get(i).collideWith(pig)) {
+                        inity = stones.get(i).curPos.y;
+                        ((AnimationDrawable)(stones.get(i).getDrawable())).start();
+                        ((AnimationDrawable)(stones.get(i).getDrawable())).setOneShot(true);
+                        stones.remove(i);
+                        break;
+                    }
+
+                    // c. Remove any obstacle that already moved out from the arena
+                    if (stones.get(i).isOutOfArena())
+                        stones.remove(i);
+
+                }
+            }
+
+            // v. Draw the game objects
+            Canvas canvas = getHolder().lockCanvas();
+            if (canvas != null) {
+                // a. Draw the scrolling background
+                background.drawOn(canvas);
+
+                // b. Draw the obstacles
+                for (int i = 0; i < stones.size(); i++) {
+                    stones.get(i).drawOn(canvas);
+                }
+
+
+                // c. Draw the flying android
+                pig.drawOn(canvas);
+
+                // d. Draw game text
+                drawGameText(canvas);
+
+                getHolder().unlockCanvasAndPost(canvas);
+            }
+        }
+
+        /** Paint object for painting text. */
+        private Paint textPaint = new Paint();
+        /** Draws text for the game. */
+        private void drawGameText(Canvas canvas) {
+            Resources res = getResources();
+            textPaint.setColor(Color.BLACK);
+            textPaint.setTextSize(TEXT_SIZE);
+
+            // Add code here
+            // Task 1: Draw game information
+            // If game over
+            // - Draw "Game Over" and the total time elapsed
+            // Else if wait for touch
+            // - Draw "Touch to Start!"
+            // Else
+            // - Draw the total time elapsed on the top left corner of the arena
+            if (gameOver) {
+                if (startTime > 0) {
+                    totalTime += (System.currentTimeMillis() - startTime);
+                    startTime = 0;
+                }
+                float gameTime = totalTime / 1000.0f;
+                textPaint.setTextSize(2 * TEXT_SIZE);
+                textPaint.setTextAlign(Paint.Align.CENTER);
+                canvas.drawText(res.getString(R.string.game_over), getWidth() / 2, getHeight() / 2, textPaint);
+                canvas.drawText(res.getString(R.string.time_elapse, gameTime), getWidth() / 2, getHeight() / 2 + (2 * TEXT_SIZE), textPaint);
+            }
+            else if (waitForTouch) {
+                textPaint.setTextSize(2 * TEXT_SIZE);
+                textPaint.setTextAlign(Paint.Align.CENTER);
+                canvas.drawText(res.getString(R.string.start), getWidth() / 2, getHeight() / 2, textPaint);
+            }
+            else {
+                textPaint.setTextSize(TEXT_SIZE);
+                textPaint.setTextAlign(Paint.Align.LEFT);
+                float gameTime = (System.currentTimeMillis() - startTime + totalTime) / 1000.0f;
+                canvas.drawText(res.getString(R.string.time_elapse, gameTime), TEXT_SIZE, TEXT_SIZE, textPaint);
+            }
+        }
+    }
+
+    /** Create obstacles randomly. */
+    public void createObstacles() {
+        // Add code here
+        // Task 2: Create one pair of pipes for every 15-25s randomly
+        float gameTime = (System.currentTimeMillis() - startTime + totalTime);
+        float timeDiff = gameTime - obstacleCreationTime;
+        if (obstacleCreationTime == -1 || timeDiff > ((Math.random()*10000) + 5000)) {
+            obstacleCreationTime = gameTime;
+            Stone s = new Stone(context);
+            stones.add(s);
+        }
+    }
+
+    /** Game over. */
+    public void gameOver() {
+        gameOver = true;
+        ((AnimationDrawable)(pig.getDrawable())).stop();
+        background.stop(true);
+    }
+
+    /** Resume or start the animation. */
+    public void resume() {
+        if (timer == null)
+            timer = new Timer();
+        timer.schedule(new AnimationTask(), 0, CYCLE_DELAY);
+    }
+
+    /** Pause or stop the animation. */
+    public void pause() {
+        totalTime += (System.currentTimeMillis() - startTime);
+        waitForTouch = true;
+
+        background.stop(true);
+        ((AnimationDrawable) (pig.getDrawable())).stop();
+
+        timer.cancel();
+        timer = null;
+    }
+
+    /**
+     * Start a new game.
+     */
+    public void newGame(boolean newGame) {
+        if (newGame) {
+            arenaWidth = getWidth();
+            arenaHeight = getHeight();
+
+
+            background = new Background(context);
+            pig = new Pig(this, context);
+
+        }
+
+        gameOver = false;
+        waitForTouch = true;
+        totalTime = 0;
+        startTime = -1;
+        obstacleCreationTime = -1;
+        for (int i = 0; i < stones.size(); i++) {
+            stones.remove(i);
+        }
+        stones.clear();
+        pig.reset();
+        ((AnimationDrawable)(pig.getDrawable())).stop();
+        background.stop(true);
+
+
+
+        inity = arenaHeight ;
+    }
+
+    /**
+     * Constructs an animation view. This performs initialization including the
+     * event handlers for key presses and touches.
+     */
     public PigView(Context context) {
         super(context);
+        this.context = context;
+
+        setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                userInput.save(event);
+                return true;
+            }
+        });
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                while (getWidth() == 0)
+                    ; // Wait for layout
+                newGame(true);
+            }
+        }, 0);
+    }
+
+    protected boolean verifyDrawable(Drawable who) {
+        super.verifyDrawable(who);
+        return who == pig.getDrawable();
+    }
+
+    public void invalidateDrawable(Drawable drawable) {
     }
 }
